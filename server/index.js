397b981ce71acd233a4cac5941b2b3eb54ed9220
Art <art@getwrecked.com>
2019-08-05 16:31:43 -0400

    implementing passport oauth

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const app     = express();
+const express        = require('express');
+const passport       = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
-});
+const app            = express();
+//new instance of passport google strategy
+passport.use(new GoogleStrategy());
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+

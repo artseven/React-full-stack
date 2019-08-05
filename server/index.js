cf7f59559950799dc136dcec3e38a6d028cb7326
Art <art@getwrecked.com>
2019-08-05 16:53:08 -0400

    moving clientid and secret into separate file

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const passport       = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app            = express();
+
 //new instance of passport google strategy
 passport.use(new GoogleStrategy());
 

e64fa32e3d1ac2dc6594413e76ccf23c54bd39d3
artseven <yabkevich.artsiom.09@gmail.com>
2019-08-06 17:39:11 -0400

    refactored with es8

--- server/services/passport.js
+++ server/services/passport.js
@@ -24,18 +24,17 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback',
         proxy: true
-    }, (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId: profile.id })
-            .then((existingUser) => {
-                if (existingUser) {
-                    //we already have a record with this profile ID
-                    done(null, existingUser);
-                } else {
-                    //we don't have a record, create new
-                    new User({ googleId: profile.id })
-                        .save()
-                        .then(user => done(null, user));
-                }
-            })
+    }, 
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleId: profile.id })
+        
+        if (existingUser) {
+            //we already have a record with this profile ID
+            done(null, existingUser);
+        } else {
+            //we don't have a record, create new
+            const user = await new User({ googleId: profile.id }).save()
+            done(null, user);
+        }
     })
 );

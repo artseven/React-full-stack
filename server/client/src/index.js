3a5948eaefad254d2c807fc27ddac6e052331817
artseven <yabkevich.artsiom.09@gmail.com>
2019-08-08 09:24:59 -0400

    implementing first reducer

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -4,9 +4,11 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 
 //dummy reducer for now as an argument
-const store = createStore(() => [], {}, applyMiddleware());
+//                          ||
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
     <Provider store={store}><App /></Provider>,

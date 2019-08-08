1606ab0149a4424f90ceb452ffbd18d54ee65368
artseven <yabkevich.artsiom.09@gmail.com>
2019-08-08 11:56:24 -0400

    refactoring into class based component

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -6,7 +6,7 @@ const fetchUser = () => {
     return function(dispatch) {
         axios.get('/api/current_user')
         .then(res => dispatch({ type: FETCH_USER, payload: res}))
-    }             
+    }                          
 };
         
                 
--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './Header';
@@ -7,21 +7,23 @@ const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
+class App extends Component {
     //materializeCSS assumes that there is at least 1 root component
     //with the class "container"
-    return (
-        <div className="container">
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route exact path="/" component={Landing} />
-                    <Route exact path="/surveys" component={Dashboard} />
-                    <Route path="/surveys/new" component={SurveyNew} />
-                </div>
-            </BrowserRouter>
-        </div>
-    );
+    render() {
+        return (
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/surveys" component={Dashboard} />
+                        <Route path="/surveys/new" component={SurveyNew} />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    };
 };
 
 export default App;

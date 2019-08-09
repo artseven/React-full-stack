5edd48e31fb8876b44bc06d47c3188cc1e800ad2
artseven <yabkevich.artsiom.09@gmail.com>
2019-08-09 09:15:42 -0400

    refactoring

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
-    //redux thunk will pass dispatch as an argument
-    return function(dispatch) {
-        axios.get('/api/current_user')
-        .then(res => dispatch({ type: FETCH_USER, payload: res}))
-    }                          
-};
+export const fetchUser = () => async dispatch => {
+    //redux thunk will pass dispatch as an argument 
+    const res = await axios.get('/api/current_user')
+    
+    dispatch({ type: FETCH_USER, payload: res});
+}                          
         
                 

ddb939e449f82b021fc0b9e0075af7147b72b1df
artseven <yabkevich.artsiom.09@gmail.com>
2019-08-09 09:48:27 -0400

    adding case for reducer

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -2,10 +2,9 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 export const fetchUser = () => async dispatch => {
-    //redux thunk will pass dispatch as an argument 
-    const res = await axios.get('/api/current_user')
+    const res = await axios.get('/api/current_user');
     
-    dispatch({ type: FETCH_USER, payload: res});
+    dispatch({ type: FETCH_USER, payload: res });
 }                          
         
                 
--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,6 +1,11 @@
-export default function(state = {}, action) {
+import { FETCH_USER } from '../actions/types';
+
+export default function(state = null, action) {
     console.log('Our action is: ', action);
     switch (action.type) {
+        case FETCH_USER:
+            //user model or false
+            return action.payload || false;
         default:
             return state;
     }

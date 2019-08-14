957b588707e51ffd53fab690953b6edb5de469fd
artseven <yabkevich.artsiom.09@gmail.com>
2019-08-14 10:06:39 -0400

    showing credits in the header

--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,7 +1,6 @@
 import { FETCH_USER } from '../actions/types';
 
 export default function(state = null, action) {
-    console.log('Our action is: ', action);
     switch (action.type) {
         case FETCH_USER:
             //user model or false

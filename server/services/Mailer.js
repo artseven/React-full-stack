906b8f8a4b8c9484f1acb79cd8984fb7d3fe3031
artseven <yabkevich.artsiom.09@gmail.com>
2019-08-27 17:02:30 -0400

    wiring up the sendgrid

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -6,7 +6,8 @@ const helper = sendgrid.mail;
 class Mailer extends helper.Mail {
 	constructor({ subject, recipients }, content) {
 		super();
-
+		//initializing sendgrid API object
+		this.sgApi = sendgrid(keys.sendGridKey);
 		this.from_email = new helper.Email('no-reply@emaily.com');
 		this.subject = subject;
 		this.body = new helper.Content('text/html', content);
@@ -14,6 +15,7 @@ class Mailer extends helper.Mail {
 
 		this.addContent(this.body);
 		this.addClickTracking();
+		this.addRecipients();
 	}
 
 	formatAddresses(recipients) {
@@ -25,9 +27,29 @@ class Mailer extends helper.Mail {
 	addClickTracking() {
 		const trackingSettings = new helper.TrackingSettings();
 		const clickTracking = new helper.ClickTracking(true, true);
-
+		
 		trackingSettings.setClickTracking(clickTracking);
 		this.addTrackingSettings(trackingSettings);
+	}
+
+	addRecipients() {
+		const personalize = new helper.Personalization();
+
+		this.recipients.forEach(recipient => {
+			personalize.addTo(recipient);
+		});
+		this.addPersonalization(personalize);
+	}
+
+	async send() {
+		const request = this.sgApi.emptyRequest({
+			method: 'POST',
+			path: '/v3/mail/send',
+			body: this.toJSON()
+		})
+
+		const response = this.sgApi.API(request);
+		return response;
 	}
 }
 

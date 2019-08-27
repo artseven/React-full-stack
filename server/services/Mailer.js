f342b088d1dd71b9cb784c9edc586c7286d316f9
artseven <yabkevich.artsiom.09@gmail.com>
2019-08-27 15:33:20 -0400

    setting up mailer class

---
+++ server/services/Mailer.js
@@ -0,0 +1,10 @@
+const sendgrid = require('sendgrid');
+const keys     = require('../config/keys');
+
+const helper = sendgrid.mail;
+
+class Mailer extends helper.Mail {
+	
+}
+
+module.exports = Mailer;

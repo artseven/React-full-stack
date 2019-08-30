fc2264659597688c84eab1b7fc8c94e1edf4c913
artseven <yabkevich.artsiom.09@gmail.com>
2019-08-30 10:56:24 -0400

    adding component-level state

--- server/client/src/components/surveys/SurveyNew.js
+++ server/client/src/components/surveys/SurveyNew.js
@@ -1,8 +1,17 @@
 //shows SurveyForm and SurveyFormReview
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+	// constructor(props) {
+	// 	super(props);
+
+	// 	this.state = { new: true };
+	// }
+	// shorter version
+	state = { showFormReview: false };
+
 	render() {
 		return (
 			<div>

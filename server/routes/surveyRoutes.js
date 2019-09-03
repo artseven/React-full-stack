6f52823b9c5087e1340d0b0d16b00c8811442ab6
artseven <yabkevich.artsiom.09@gmail.com>
2019-09-03 13:30:06 -0400

    filtering out dirty webhook data

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,5 +1,5 @@
 const _ 						 = require('lodash');
-const Path           = require('path-parser');
+const Path           = require('path-parser').default;
 const { URL } 			 = require('url');
 const mongoose       = require('mongoose');
 const requireLogin   = require('../middlewares/requireLogin');
@@ -16,10 +16,22 @@ module.exports = app => {
 	});
 
 	app.post('/api/surveys/webhooks', (req, res) => {
-		const events = _.map(req.body, (event) => {
-			const pathname = new URL(event.url).pathname;
+		const events = _.map(req.body, ({ email, url }) => {
+			const pathname = new URL(url).pathname;
 			const p = new Path('/api/surveys/:surveyId/:choice');
-		})
+			const match = p.test(pathname);
+
+			if (match) {
+				return { email, surveyId: match.surveyId, choice: match.choice };
+			}
+		});
+
+		const compactEvents = _.compact(events);
+		const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+
+		console.log(uniqueEvents);
+
+		res.send({});
 	})
 
 

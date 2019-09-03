7a3260fbf531c8f8ff79c0b7509ad968f5bc35b0
artseven <yabkevich.artsiom.09@gmail.com>
2019-09-03 15:05:46 -0400

    optimizing db query

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -18,7 +18,7 @@ module.exports = app => {
 	app.post('/api/surveys/webhooks', (req, res) => {
 		const p = new Path('/api/surveys/:surveyId/:choice');
 		
-		const events = _.chain(req.body)
+		_.chain(req.body)
 			.map(({ email, url }) => {
 				const match = p.test(new URL(url).pathname);
 				if (match) {
@@ -27,9 +27,18 @@ module.exports = app => {
 			})
 			.compact()
 			.uniqBy('email', 'surveyId')
+			.each(event => {
+				Survey.updateOne({
+					_id: surveyId,
+					recipients: {
+						$elemMatch: { email: email, responded: false }
+					}
+				}, {
+					$inc: { [choice]: 1 },
+					$set: { 'recipients.$.responded': true }
+				})
+			})
 			.value();
-
-		console.log(events);
 
 		res.send({});
 	})
